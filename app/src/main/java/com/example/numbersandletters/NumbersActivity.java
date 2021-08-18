4d02c25d36475146f02c42c4ea7aee7f044cd31b
Jadabyte <thibaud.streignart@gmail.com>
2021-08-18 18:03:43 +0200

    NumbersActivity now generates high and low numbers + displays the list of selected numbers

--- app/src/main/java/com/example/numbersandletters/NumbersActivity.java
+++ app/src/main/java/com/example/numbersandletters/NumbersActivity.java
@@ -1,12 +1,17 @@
 package com.example.numbersandletters;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.TypedArrayUtils;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 
@@ -14,8 +19,9 @@ public class NumbersActivity extends AppCompatActivity {
 
     private Button mHighNumber;
     private Button mLowNumber;
+    private Button mContinue;
     private TextView mSelectedNumbers;
-    private int[] selectedNumbers;
+    private List<String> selectedNumbers;
     private int[] highNumberArray = {10, 25, 50, 100};
 
     @Override
@@ -25,14 +31,41 @@ public class NumbersActivity extends AppCompatActivity {
 
         mSelectedNumbers = (TextView) findViewById(R.id.tv_selected_numbers);
         mHighNumber = (Button) findViewById(R.id.b_high_number);
+        mContinue = (Button) findViewById(R.id.b_continue);
+        selectedNumbers = new ArrayList<String>();
 
         mHighNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int random = new Random().nextInt(highNumberArray.length);
-                mSelectedNumbers.setText(String.valueOf(highNumberArray[random]));
+                selectedNumbers.add(String.valueOf(highNumberArray[random]));
+                mSelectedNumbers.setText(String.valueOf(selectedNumbers));
+
+                //Toast.makeText(getBaseContext(), String.valueOf(selectedNumbers) , Toast.LENGTH_SHORT ).show();
+                if(selectedNumbers.size() == 9){
+                    mHighNumber.setVisibility(View.INVISIBLE);
+                    mLowNumber.setVisibility(View.INVISIBLE);
+                    mContinue.setVisibility(View.VISIBLE);
+                }
             }
         });
+
+        mLowNumber = (Button) findViewById(R.id.b_low_number);
+        mLowNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int random = new Random().nextInt(9 - 1 + 1) + 1;
+                selectedNumbers.add(String.valueOf(random));
+                mSelectedNumbers.setText(String.valueOf(selectedNumbers));
+
+                if(selectedNumbers.size() == 9){
+                    mHighNumber.setVisibility(View.INVISIBLE);
+                    mLowNumber.setVisibility(View.INVISIBLE);
+                    mContinue.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
 
     }
 }
